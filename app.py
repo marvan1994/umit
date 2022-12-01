bed647e8434a4045debc224d4a54271fe525992b
mars <mars.shakirov@gmail.com>
2022-12-01 04:21:50 +0300

    new app added

--- app.py
+++ app.py
@@ -1,11 +1,156 @@
+import streamlit as st
+
 import pandas as pd
+
 import json
-import os
+
+from load_css import local_css
+
+import pathlib
+
+st.set_page_config(page_title='Привязка Подтем',
+    page_icon="um.ico",)
+
+local_css("style.css")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            section {background-color:#F2D3EB;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+all_subject_names = ['ang_ege', 'baz_ege', 'bio_ege', 'geo_ege', 'inf_ege', 'ist_ege', 'lit_ege', 'mat_ege',
+                         'nem_ege', 'rus_ege', 'fiz_ege', 'him_ege', 'obs_ege']
+
+def reset_session():
+    st.session_state.subtheme = []
+
+
+df = pd.read_csv('subtheme_sheet.csv', dtype=str)
+df = df[['subject', 'class_year', 'section', 'subtheme', 'subtheme_id']]
+df['class_year'] = df.class_year.map({'3': 'ege', '1': 'oge', '4': '10c'})
+
+def show():
+
+    st.sidebar.image('um.png', caption=None, width = 100, use_column_width='True', clamp=False, channels="RGB", output_format="auto")
+
+
+    st.write(
+        """
+        ## Это программа для привязывания подтем
+        """
+    )
+
+    def subtheme_add(subject_name,subject_info):
+
+
+        df.fillna('', inplace=True)
+        st.dataframe(df.query(f'subject == "{subject_name[0:3]}" and class_year == "{subject_name[-3:]}"'))
+
+
+        if 'subtheme' not in st.session_state:
+             st.session_state.subtheme = []
+
+        section_names = list(subject_info['sections'].keys())
+        section = st.selectbox(label = 'Выберите раздел', options = ['']+section_names , key = subject_name)
+
+        if section !='':
+
+            subtheme_number_to_name = {key: subject_info['subtheme_names'][key] for key in subject_info['subtheme_names'].keys()}
+            subtheme_name_to_number = {value: key for (key, value) in subtheme_number_to_name.items()}
+
+
+            section_number_list = subject_info['sections'][section]
+
+            section_name_list = [subtheme_number_to_name[subtheme] for subtheme in section_number_list]
+
+            for i in range(len(section_number_list)):
+
+                if int(section_number_list[i]) in st.session_state.subtheme:
+
+                    cheker = st.checkbox(f'{section_name_list[i]} ({int(section_number_list[i])})', key = subject_name+'_'+str(section_number_list[i]), value= True)
+
+                else:
+                    cheker = st.checkbox(f'{section_name_list[i]} ({int(section_number_list[i])})', key = subject_name+'_'+str(section_number_list[i]))
+
+
+                if cheker:
+
+                    current_state = list(set(st.session_state.subtheme))
+                    current_state.extend([int(section_number_list[i])])
+                    current_state = list(set(current_state))
+                    st.session_state.subtheme = current_state
+
+                else:
+                    current_state = list(set(st.session_state.subtheme))
+
+                    if int(section_number_list[i]) in st.session_state.subtheme:
+                        current_state = st.session_state.subtheme
+                        current_state.remove(int(section_number_list[i]))
+                        st.session_state.subtheme = current_state
 
 
 
-with open(r'umit_info_spec.json', 'r') as f:
-    main_dict = json.loads(f.read())
+
+    with open('subtheme_info.json', 'r') as f:
+        subtheme_info = json.loads(f.read())
+
+    subject_names = subtheme_info.keys()
 
 
-df_dict = {file.split('_')[1].split('.')[0]:pd.read_excel(f'./umit_files/{file}') for file in os.listdir('umit_files')}
+    subjects_inrus = ['Английский', "Базовая", "Биология", "География", "Информатика", "История", "Литература",
+                      "Математика", "Немецкий", "Русский", "Физика", "Химия", 'Обществознание']
+    subjects_ineng = ['ang', 'baz', 'bio', 'geo', 'inf', 'ist', 'lit', 'mat', 'nem', 'rus', 'fiz', 'him', 'obs']
+    subject_dict = dict(zip(subjects_inrus, subjects_ineng))
+    degree_dict = {'ЕГЭ':'ege', 'ОГЭ':'oge','10 класс':'10c'}
+
+
+    subject = st.sidebar.selectbox('Выберите предмет', [''] + list(subject_dict.keys()))
+    degree = st.sidebar.radio('Выберите направление', ['ЕГЭ', 'ОГЭ','10 класс'])
+
+
+    try:
+        subject_name = subject_dict[subject]+'_'+degree_dict[degree]
+        if subject_name in subject_names:
+
+            st.markdown(f'##### Подтемы по предмету {subject} {degree}')
+
+            subject_info = subtheme_info[subject_name]
+            subtheme_add(subject_name, subject_info)
+
+        else:
+
+            st.markdown(f'##### Подтем по направлению {degree} по предмету {subject} не существует')
+    except Exception as e:
+        #print(str(e))
+        pass
+    show_subtheme()
+
+
+def show_subtheme():
+
+    t1 = st.sidebar.empty()
+    t1.markdown('# Здесь будут отображаться ID подтем')
+    if 'subtheme' in st.session_state:
+
+        current_state = st.session_state.subtheme
+
+        if len(current_state) !=0:
+
+            current_state.sort()
+            current_state_str = [str(item) for item in current_state]
+            t1.text_area(label = 'ID подтем можно копировать отсюда',value=','.join(current_state_str))
+            istrue = st.sidebar.button('Очистить подтемы')
+            if istrue:
+                reset_session()
+                current_state_str = [str(item) for item in st.session_state.subtheme]
+                t1.markdown(','.join(current_state_str))
+
+
+show()
+
+
--- temporary_file.py
+++ temporary_file.py
@@ -4,48 +4,64 @@ import json
 
 import  os
 
-path_to_umit_files = '.\\umit_files\\'
-
-subjects_inrus = ['Английский', "Базовая", "Биология", "География", "Информатика", "История", "Литература", "Математика", "Немецкий", "Русский", "Физика", "Химия",'Обществознание']
-subjects_ineng = ['ang','baz','bio','geo','inf','ist','lit','mat','nem','rus','fiz','him','obs']
-subject_names_ege = ['ang_ege','baz_ege','bio_ege','geo_ege','inf_ege','ist_ege','lit_ege','mat_ege','nem_ege','rus_ege','fiz_ege','him_ege','obs_ege']
-subject_names_oge = ['ang_oge','bio_oge','geo_oge','inf_oge','ist_oge','lit_oge','mat_oge','nem_oge','rus_oge','fiz_oge','him_oge','obs_oge']
-
-subject_names_r2e = dict(zip(subjects_inrus,subjects_ineng))
-subject_names_e2r = dict(zip(subjects_ineng,subjects_inrus))
-
-all_subject_names = subject_names_ege + subject_names_oge
 
 
-main_dict = dict(zip(all_subject_names, ['' for x in range(len(all_subject_names))]))
-
-list_dir = os.listdir(path_to_umit_files)
-
-for sub in all_subject_names:
-
-    df = pd.read_excel(f'.\\umit_files\\umit_{sub[0:3]}.xlsx')
-    print(sub)
-    N_sections = sum(1 for x in df.columns[0::2].to_list() if 'Unnamed' not in x)
-    section_names = df.columns[0:2 * N_sections:2]
-    id_columns = df.columns[1:2 * N_sections:2]
-    umit_ids = []
-    umit_names = []
-    for x in range(N_sections):
-        umit_names.extend(df[section_names[x]].dropna().to_list())
-        section_ids = [str(int(x)) for x in df[id_columns[x]].dropna().to_list()]
-        umit_ids.extend(section_ids)
-    all_umit_names = dict(zip(umit_ids, umit_names))
-
-    sections = {section_names[x]: [str(int(x)) for x in df[id_columns[x]].dropna().to_list()] for x in
-                range(N_sections)}
-
-    subject_info = {'umit_names': all_umit_names, 'sections': sections}
-
-    subject_name = sub
-    main_dict[subject_name] = subject_info
-
-with open('umit_info.json', 'w+') as f:
-    json.dump(main_dict, f)
+allsubs = ['ang_ege', 'ang_oge', 'baz_ege', 'bio_ege', 'bio_oge', 'fiz_ege',
+       'geo_ege', 'geo_oge', 'him_ege', 'him_oge', 'inf_ege', 'inf_oge',
+       'ist_ege', 'ist_oge', 'lit_ege', 'lit_oge', 'mat_ege', 'mat_oge',
+       'nem_ege', 'obs_ege', 'obs_oge', 'rus_oge', 'fiz_oge', 'ang_10c',
+       'bio_10c', 'fiz_10c', 'him_10c', 'ist_10c', 'lit_10c', 'mat_10c',
+       'obs_10c', 'rus_10c', 'rus_ege', 'inf_10c']
 
 
+df = pd.read_csv('subtheme_sheet.csv')
+df = df[['subject','class_year','section','subtheme','subtheme_id']].drop_duplicates()
+df.class_year = df.class_year.map({3:'ege',1:'oge',4:'10c'})
+df['sub_deg'] = df['subject']+'_'+df['class_year']
 
+
+main_dict = dict(zip(allsubs, ['' for x in range(len(allsubs))]))
+
+for sub_deg in allsubs:
+    dft = df.query(f'sub_deg == "{sub_deg}"')
+    sections = dft.section.unique().tolist()
+
+    all_subtheme_names = dict(zip(dft.subtheme_id.tolist(),dft.subtheme.tolist()))
+    sections_dict = { section: [str(int(x)) for x in dft.query(f'section == @section').subtheme_id.to_list()] for section in sections}
+
+    subject_info = {'subtheme_names': all_subtheme_names, 'sections': sections_dict}
+
+    main_dict[sub_deg] = subject_info
+
+    with open(r'subtheme_info.json', 'w+') as f:
+
+        json.dump(main_dict,f)
+
+# for sub in all_subject_names:
+#
+#     df = pd.read_excel(f'.\\umit_files\\umit_{sub[0:3]}.xlsx')
+#     print(sub)
+#     N_sections = sum(1 for x in df.columns[0::2].to_list() if 'Unnamed' not in x)
+#     section_names = df.columns[0:2 * N_sections:2]
+#     id_columns = df.columns[1:2 * N_sections:2]
+#     umit_ids = []
+#     umit_names = []
+#     for x in range(N_sections):
+#         umit_names.extend(df[section_names[x]].dropna().to_list())
+#         section_ids = [str(int(x)) for x in df[id_columns[x]].dropna().to_list()]
+#         umit_ids.extend(section_ids)
+#     all_umit_names = dict(zip(umit_ids, umit_names))
+#
+#     sections = {section_names[x]: [str(int(x)) for x in df[id_columns[x]].dropna().to_list()] for x in
+#                 range(N_sections)}
+#
+#     subject_info = {'umit_names': all_umit_names, 'sections': sections}
+#
+#     subject_name = sub
+#     main_dict[subject_name] = subject_info
+#
+# with open('umit_info.json', 'w+') as f:
+#     json.dump(main_dict, f)
+#
+#
+#
