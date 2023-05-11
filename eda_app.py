495946b17be2e56a6a939055892e0b768ccc0844
mars <mars.shakirov@gmail.com>
2023-05-11 03:08:29 +0300

    added eda retro app

---
+++ eda_app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
+from load_css import local_css
+
+
+st.set_page_config(page_title='Ретроспектива 22-23',
+    page_icon="um.ico",)
+
+local_css("style.css")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv(file)
+    return df
+
+col_names = {
+    "class_degree":"Класс","speaker":"Спикер",
+    "sept":"Сентябрь","oct":"Октябрь","nov":"Ноябрь","dec":"Декабрь",
+    "jan":"Январь","feb":"Февраль","mart":"Март","apr":"Апрель",
+    "may":"Май","subject":"Предмет"}
+
+
+file = "rp.csv"
+df = load_data()
+df = df.rename(columns = col_names)
+
+st.title("Ретроспектива покупок")
+st.write('Добро пожаловать в ретроспективу покупок. Здесь можно быстро и эффективно отфильтровать покупки за весь год.')
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+
+    modify = st.checkbox("Добавить фильтры")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Фильтрация по полю - ", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 20:
+                user_cat_input = right.multiselect(
+                    f"Данные по полю {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Данные по полю {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Данные по полю {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Используйте regex или кусок текста для поля {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
+
+st.dataframe(filter_dataframe(df))
